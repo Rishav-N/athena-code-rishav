2a026e0f5bde42b46c2ad5d251b67a380c6e4a1a
Rishav-N <rishav.a.nair@gmail.com>
2026-05-10 19:09:25 +0000

    removed unnecessary files

---
+++ src/subsystems/navigation/athena_planner/include/athena_planner/led_status_node.hpp
@@ -0,0 +1,41 @@
+#ifndef ATHENA_PLANNER__LED_STATUS_NODE_HPP_
+#define ATHENA_PLANNER__LED_STATUS_NODE_HPP_
+
+#include <string>
+
+#include "behaviortree_cpp_v3/action_node.h"
+#include "rclcpp/rclcpp.hpp"
+
+// Change this include if your msg package name is different
+#include "msgs/msg/led_status.hpp"
+
+namespace bt_nodes
+{
+
+class LedStatusNode : public BT::SyncActionNode
+{
+public:
+  LedStatusNode(
+    const std::string & name,
+    const BT::NodeConfiguration & conf);
+
+  BT::NodeStatus tick() override;
+
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<std::string>("color"),
+      BT::InputPort<std::string>("topic_name", "/led_status")
+    };
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Publisher<msgs::msg::LedStatus>::SharedPtr led_pub_;
+
+  std::string topic_name_;
+};
+
+}  // namespace bt_nodes
+
+#endif  // ATHENA_PLANNER__LED_STATUS_NODE_HPP_
---
+++ src/subsystems/navigation/athena_planner/plugins/led_status_node.cpp
@@ -0,0 +1,65 @@
+#include "athena_planner/led_status_node.hpp"
+
+namespace bt_nodes
+{
+
+LedStatusNode::LedStatusNode(
+  const std::string & name,
+  const BT::NodeConfiguration & conf)
+: BT::SyncActionNode(name, conf)
+{
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+
+  topic_name_ = "/led_status";
+  getInput("topic_name", topic_name_);
+
+  led_pub_ = node_->create_publisher<msgs::msg::LedStatus>(
+    topic_name_,
+    10);
+}
+
+BT::NodeStatus LedStatusNode::tick()
+{
+  std::string color;
+  if (!getInput("color", color)) {
+    RCLCPP_ERROR(node_->get_logger(), "LedStatusNode missing required input port [color]");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  msgs::msg::LedStatus msg;
+  msg.cmd = msgs::msg::LedStatus::CMD_SOLID;
+  msg.r = 0;
+  msg.g = 0;
+  msg.b = 0;
+  msg.param = 0;
+
+  if (color == "red" || color == "RED") {
+    msg.r = 255;
+    msg.g = 0;
+    msg.b = 0;
+  } else if (color == "green" || color == "GREEN") {
+    msg.r = 0;
+    msg.g = 255;
+    msg.b = 0;
+  } else {
+    RCLCPP_ERROR(
+      node_->get_logger(),
+      "Invalid LED color [%s]. Expected red or green.",
+      color.c_str());
+    return BT::NodeStatus::FAILURE;
+  }
+
+  led_pub_->publish(msg);
+
+  RCLCPP_INFO(
+    node_->get_logger(),
+    "Published LED status: color=%s rgb=[%u, %u, %u]",
+    color.c_str(),
+    msg.r,
+    msg.g,
+    msg.b);
+
+  return BT::NodeStatus::SUCCESS;
+}
+
+}  // namespace bt_nodes
